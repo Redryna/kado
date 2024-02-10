86f42f049e7ce018718148f94ada38c4ee20b8c9
Redryna <93069442+Redryna@users.noreply.github.com>
2024-02-10 10:47:08 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+
+        document.addEventListener("DOMContentLoaded", function() {
+            setTimeout(function() {
+                window.location.href = "index.html";
+                alert("maaf,kado cuma itu doang 😅");
+                window.close();
+            }, 15000); // 15000 milidetik = 15 detik
+        });
