4417bc6da80aaeaf28fd80666866c0adf07e44d1
Redryna <93069442+Redryna@users.noreply.github.com>
2024-02-10 12:44:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ onload = () =>{
         document.addEventListener("DOMContentLoaded", function() {
             setTimeout(function() {
                 window.location.href = "index.html";
-                alert("maaf,kado cuma itu doang 😅");
+                alert("maaf,kadonya cuma itu doang hehe😅");
                 window.close();
-            }, 15000); // 15000 milidetik = 15 detik
+            }, 12000);
         });
